c8fe78998cb2b13958b31d91df7132f1e0a8439d
Daria Bulavina <ms.pankina.97@yandex.ru>
2024-06-11 23:26:35 +0300

    Прописан функционал модального окна (первые 4 шага)

--- src/index.js
+++ src/index.js
@@ -24,3 +24,58 @@ const createCard = (card, removeCard) => {
 initialCards.forEach((card) => {
   cardContainer.append(createCard(card, removeCard));
 });
+
+////////
+
+const editButton = document.querySelector('.profile__edit-button');
+editButton.addEventListener('click', function(evt){
+  const editForm = document.querySelector('.popup_type_edit');
+  openModal(editForm);
+  const closeButton = document.querySelector('.popup__close');
+  closeButton.addEventListener('click', function(evt) {
+    closeModal(editForm);
+  });
+  document.addEventListener('keydown', function(evt) {
+    if (evt.key === 'Escape') {
+      closeModal(editForm);
+    }
+  });
+  document.addEventListener('click', function(evt){
+    if (evt.target === editForm) {
+      closeModal(editForm);
+    }
+  });
+
+  // Находим поля формы в DOM
+  const nameInput = document.querySelector('.popup__input_type_name');
+  const jobInput = document.querySelector('.popup__input_type_description');
+  
+  const profileTitle = document.querySelector('.profile__title');
+  const profileDescription = document.querySelector('.profile__description');
+
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
+  // Обработчик «отправки» формы, хотя пока
+  // она никуда отправляться не будет
+  function handleFormSubmit(evt) {
+      evt.preventDefault();
+      const name = nameInput.value;
+      const job = jobInput.value;
+  
+      profileTitle.textContent = name;
+      profileDescription.textContent = job;
+      closeModal(editForm);
+  }
+  
+  // Прикрепляем обработчик к форме:
+  // он будет следить за событием “submit” - «отправка»
+  editForm.addEventListener('submit', handleFormSubmit);
+});
+
+function openModal(element) {
+  element.classList.add('popup_is-opened');
+};
+
+function closeModal(element) {
+  element.classList.remove('popup_is-opened');
+};
